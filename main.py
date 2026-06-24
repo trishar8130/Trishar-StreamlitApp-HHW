ea7c7042bc824ba2bb98f1c83f66fd03b41d2d06
trishar8130 <trishar8130@gmail.com>
2026-06-24 09:36:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.title("Welcome to AI")
+student_name = st.text_input("Enter Student Name")
+if student_name:
+    st.write(f"Welcome, {student_name}!")
+if st.button("Show Details"):
+    st.write("Student Name:", student_name)
+    st.write("Class: XII")
+    st.write("Section: A")
